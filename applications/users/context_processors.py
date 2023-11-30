66adbf127ac2350a83f092430a3d2555457e8616
Carrington-dev <crn96m@gmail.com>
2023-11-30 15:40:26 +0200

    Final Setup PoolMasters @ 30 Nov

--- applications/users/context_processors.py
+++ applications/users/context_processors.py
@@ -2,7 +2,7 @@ def important(request):
     context =  dict()
     context['company'] = 'PoolMasters'
     context['email'] = 'info@poolmasters.co.za'
-    context['email2'] = 'carrie@poolmasters.co.za'
+    context['email2'] = 'admin@poolmasters.co.za'
     context['tel1'] = '+27 67 735 2242'
     context['tel2'] = '+27 63 859 9481'
     return context
